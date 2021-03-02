545ea2815f853fc8344b5de42fc3e9e979792247
kazuki0925 <kazukinyo0925@gmail.com>
2021-03-02 17:51:00 +0900

    firstquiz

---
+++ app.js
@@ -0,0 +1,37 @@
+const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
+const answers = [
+  'スーパーファミコン',
+  'プレイステーション2',
+  'ニンテンドースイッチ',
+  'ニンテンドーDS'
+];
+const correct = 'ニンテンドーDS';
+
+const $button = document.getElementsByTagName('button');
+let buttonLength = $button.length;
+
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question;
+  let buttonIndex = 0;
+  while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  }
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+  if(correct === e.target.textContent) {
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+};
+
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+  $button[handlerIndex].addEventListener('click', (e) => {
+    clickHandler(e);
+  });
+  handlerIndex++;
+}
